29ce755d19fb2b0b269693a81c2b1e25f9fbcc6d
ingTorok <toroklehel89@gmail.com>
2019-02-09 12:22:43 +0100

    added default values to database at seed

--- OopRestaurant/Migrations/Configuration.cs
+++ OopRestaurant/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace OopRestaurant.Migrations
 {
+    using OopRestaurant.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -18,6 +19,58 @@ namespace OopRestaurant.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            var category1 = new Category() { Name = "Levesek" }; //Id-t az Entity Framework ossza ki
+            var category2 = new Category() { Name = "Hideg Eloetelek" };
+            var category3 = new Category() { Name = "Meleg eloetelek" };
+
+            context.Categories.AddOrUpdate(x => x.Name, category1, category2, category3);
+
+            context.MenuItems.AddOrUpdate(x => x.Name, new MenuItem()
+            {
+                Name = "Tengeri hal Trio",
+                Description = "Atlanti lazactar, pacolt lazacfile es tonhal lazackarikaval",
+                Price = 7500,
+                Category = category1
+            });
+
+            context.MenuItems.AddOrUpdate(x => x.Name, new MenuItem()
+            {
+                Name = "Borjueszencia",
+                Description = "Zoldseges gyogytyuk galuska",
+                Price = 4500,
+                Category = category1
+            });
+
+            context.MenuItems.AddOrUpdate(x => x.Name, new MenuItem()
+            {
+                Name = "Szarvasgomba cappuccino",
+                Price = 4500,
+                Category = category1
+            });
+
+            context.MenuItems.AddOrUpdate(x => x.Name, new MenuItem()
+            {
+                Name = "Hirtelen sult fogasderek illatos erdei gombakkal",
+                Price = 4500,
+                Category = category3
+            });
+
+            context.MenuItems.AddOrUpdate(x => x.Name, new MenuItem()
+            {
+                Name = "Gundel Karoly gulyaslevese 1910",
+                Price = 3500,
+                Category = category1
+            });
+
+            context.MenuItems.AddOrUpdate(x => x.Name, new MenuItem()
+            {
+                Name = "Szaritott erlelt belszin carpaccio",
+                Description = "Oreg trappista sajt, keseru levelek",
+                Price = 5000,
+                Category = category2
+            });
+
         }
     }
 }
